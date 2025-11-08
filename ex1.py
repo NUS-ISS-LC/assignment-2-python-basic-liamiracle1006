7ee21492caddb484cc60c814e30c48973d2f5280
liamiracle1006 <miraclelia1006@gmail.com>
2025-11-08 11:17:07 +0000

    Wang Ziyu

--- ex1.py
+++ ex1.py
@@ -1,3 +1,12 @@
 def find(s, n):
-# write your implementation here
+    hash_map = {}
+    
+    for i in range(len(s)):
+        complement = n - s[i]
+        
+        if complement in hash_map:
+            return [hash_map[complement], i]
+        
+        hash_map[s[i]] = i
+    
     return None
